8f9e200c83d194d46c1b837d3bde0d8566d0cdc8
AnnMarkham <ann.e.markham@outlook.com>
2021-01-15 21:24:56 -0700

    user, post & comment routes working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,13 +2,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes'); 
-// add require postRoutes & commentRoutes when created. 
-
+const postRoutes = require('./post-routes')
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
-
-// add router.use postRoutes and commentRoutes when created
-
-
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
