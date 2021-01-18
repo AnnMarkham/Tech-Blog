f363f4650072252aba762b17903e1ae18735c5aa
AnnMarkham <ann.e.markham@outlook.com>
2021-01-17 22:29:42 -0700

    update jawsdb url?

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ let sequelize;
 
 // create connection to our db
 if( process.env.JAWSDB_URL ) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_ROSE_URL);
 }
 else {
   sequelize =new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
